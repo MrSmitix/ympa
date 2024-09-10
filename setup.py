e76fd6262755451e3c8803b2e13b27ecf5954a28
root <root@mjvopoyrjs>
2024-09-10 04:09:24 +0000

    New version from 29.08.2024

---
+++ setup.py
@@ -0,0 +1,37 @@
+import sys
+from setuptools import setup, find_packages
+
+NAME = "ympa_python_flask_server"
+VERSION = "24.08.29"
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+    "connexion>=2.0.2",
+    "swagger-ui-bundle>=0.0.2",
+    "python_dateutil>=2.6.0"
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Партнерский API Маркета",
+    author_email="",
+    url="",
+    keywords=["OpenAPI", "Партнерский API Маркета"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    package_data={'': ['openapi/openapi.yaml']},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['ympa_python_flask_server=ympa_python_flask_server.__main__:main']},
+    long_description="""\
+    API Яндекс Маркета помогает продавцам автоматизировать и упростить работу с маркетплейсом.  В числе возможностей интеграции:  * управление каталогом товаров и витриной,  * обработка заказов,  * изменение настроек магазина,  * получение отчетов. 
+    """
+)
+
